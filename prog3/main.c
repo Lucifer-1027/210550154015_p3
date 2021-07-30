9d5fdb4b15a1c18f904029c3e43d009afe0f5c7b
Sudhanshu Ranjan <sudhanshuranjan757@gmail.com>
2021-07-30 12:35:30 +0530

    Errors corrected

--- prog3/main.c
+++ prog3/main.c
@@ -5,15 +5,16 @@ Child 1 searches for a word from the top of the file and child 2 search from bot
 The word is provided by parent Process.
 Whichever child finds it prints the message "Found" */
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<unistd.h>
 
 int main(int argc, char const *argv[])
 {
     pid_t id1;
     pid_t id2;
-    char key = "E" ;
+    char key;
 
     id1 = fork();
     id2 = fork();
@@ -22,78 +23,64 @@ int main(int argc, char const *argv[])
     {
         printf("Parent Process\n");
         
-        char str[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
-        int n = 10;
-        int flag;
+        char str[100];
         int i;
 
-        for(i=0; i<n; i++)
+        printf("Enter the string:");
+        gets(str);
+        printf("Enter character to be searched:");
+        key=getchar();
+
+        for(i=0; str[i]; i++)
         {
-            if (a[i] != key)
+            if (str[i] == key)
             {
-                flag = 0;
+                printf("Key '%c' is present in the string and found at location: %d\n", key, i);
             }
 
             else
-            {
-                flag = 1;
-            }
-
-            if (flag == 0)
             {
                 printf("Key is not present in the string\n");
             }
 
-            else
-            {
-                printf("Key is present in the string\n");
-            }
         }
-
+    }
+    
     else
     {
         printf("Child Process");
-    }
-
     }
     
     if (id2 > 0)
     {
         printf("Parent Process\n");
         
-        char str[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
-        int n = 0;
-        int flag;
+        char str[100];
         int i;
 
-        for(i=9; i>=n; i--)
+        printf("Enter the string:");
+        gets(str);
+        printf("Enter character to be searched:");
+        key=getchar();
+
+        for(i=0; str[i]; i++)
         {
-            if (a[i] != key)
+            if (str[i] == key)
             {
-                flag = 0;
+                printf("Key '%c' is present in the string and found at location: %d\n", key, i);
             }
 
             else
-            {
-                flag = 1;
-            }
-
-            if (flag == 0)
             {
                 printf("Key is not present in the string\n");
             }
 
-            else
-            {
-                printf("Key is present in the string\n");
-            }
         }
+    }
 
     else
     {
         printf("Child Process");
-    }
-
     }
 
     return 0;
